12c71f0779061fe09576af66da74c33cde87905f
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-04-11 14:44:38 +0500

    changes

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,8 @@ router.get('/getAllUsers', user_controller.getAllUsers);
 router.get('/details', user_controller.getUserDetails);
 router.post('/follow', verifySignature, user_controller.followUser);
 router.post('/unfollow', verifySignature, user_controller.unFollowUser);
-router.get('/getFollowers', user_controller.getUserFollowers);
-router.get('/getFollowing', user_controller.getUserFollowing);
+router.get('/getFollowers/:address', user_controller.getUserFollowers);
+router.get('/getFollowing/:address', user_controller.getUserFollowing);
 
 module.exports = {
   router: router,
