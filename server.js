ab3e40e0e62b3c207a5bf7b6c7dbc38a3b9db483
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-04-11 16:30:32 +0500

    changes

--- server.js
+++ server.js
@@ -1,48 +1,41 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const auth_route = require("./routes/auth");
-const likes_route = require("./routes/likes");
-const post_route = require("./routes/post");
-const user_route = require("./routes/user");
-const comment_route = require("./routes/comment");
+const auth_route = require('./routes/auth');
+const likes_route = require('./routes/likes');
+const post_route = require('./routes/post');
+const user_route = require('./routes/user');
+const comment_route = require('./routes/comment');
 
-const cors = require("cors");
-const Post = require("./models/post");
-const axios = require("axios");
-const User = require("./models/user");
+const cors = require('cors');
 
-const PostChanges = require("./models/PostChages");
-const UserChanges = require("./models/UserChanges");
+require('./cronjob');
 
-// ! FOR TESTING ONLY
-// require("./cronjob");
-
-const host = "0.0.0.0";
+const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("uploads"));
+app.use(express.static('uploads'));
 
 const dbURI =
-  "mongodb+srv://admin:admin123@cluster0.4h1l4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  'mongodb+srv://admin:admin123@cluster0.4h1l4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
+  .then(result => {
     app.listen(port, host, () => {
-      console.log("Server is up and running on Port:", port);
+      console.log('Server is up and running on Port:', port);
     });
   })
-  .catch((error) => {
+  .catch(error => {
     console.log(error);
   });
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   // let posts = await User.find({});
   // await posts[0].remove();
   // await posts[1].remove();
@@ -69,11 +62,11 @@ app.get("/", async (req, res) => {
   // let userChages = new UserChanges({ count: 0 });
   // userChages.save();
 
-  res.send("server live!");
+  res.send('server live!');
 });
 
-app.use("/auth", auth_route.router);
-app.use("/likes", likes_route.router);
-app.use("/posts", post_route.router);
-app.use("/users", user_route.router);
-app.use("/comment", comment_route.router);
+app.use('/auth', auth_route.router);
+app.use('/likes', likes_route.router);
+app.use('/posts', post_route.router);
+app.use('/users', user_route.router);
+app.use('/comment', comment_route.router);
