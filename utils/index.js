46b197e194a6b0e05944ff42cbde4a93483858a4
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-04-12 15:05:52 +0500

    changes

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,12 @@
 const keccak = require('keccak256');
+const Web3 = require('web3');
 
 module.exports = {
   createSignature: data => {
+    const web3 = new Web3(
+      'https://rinkeby.infura.io/v3/7c4e9e4322bc446195e561d9ea27d827',
+    );
+
     const hash = keccak(data).toString('hex');
     const signedObject = web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY);
 
