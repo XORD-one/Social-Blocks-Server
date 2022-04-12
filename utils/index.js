2ded13ce5f102fa1a472346001399e8197e03938
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-04-12 14:40:29 +0500

    changes

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,9 @@
 const keccak = require('keccak256');
-const { privateKey } = require('../config.json');
 
 module.exports = {
   createSignature: data => {
     const hash = keccak(data).toString('hex');
-    const signedObject = web3.eth.accounts.sign(hash, privateKey);
+    const signedObject = web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY);
 
     return { hash, signedObject };
   },
