fc668b8521af8742ac19ca3e16596e0d2d4c311d
ijlal-ishaq <ijlalishaq2019@gmail.com>
2022-04-14 01:34:12 +0500

    merged

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 const Likes = require('../models/likes');
 const Post = require('../models/post');
 const User = require('../models/user');
+=======
+const Likes = require("../models/likes");
+const Post = require("../models/post");
+const User = require("../models/user");
+const Comment = require("../models/Comment");
+const axios = require("axios");
+const { json } = require("body-parser");
+>>>>>>> Stashed changes
 
 // const URL = 'https://api.thegraph.com/subgraphs/name/ijlal-ishaq/social-blocks';
 
@@ -96,12 +105,60 @@ const getSinglePost = async (req, res) => {
   }
 };
 
-const removeAllPosts = async (req, res) => {
+const removeAllPostsAndUsers = async (req, res) => {
   try {
     await Post.deleteMany({});
+<<<<<<< Updated upstream
     res.status(200).json({ message: 'All posts deleted' });
+=======
+    await User.deleteMany({});
+    await Likes.deleteMany({});
+    await Comment.deleteMany({});
+
+    res.status(200).json({ message: "All posts deleted" });
+>>>>>>> Stashed changes
   } catch (err) {
     console.log(err);
+  }
+};
+
+const getTransferHistory = async (req, res) => {
+  let id = req.params.id;
+
+  const result = await axios.post(
+    "https://api.thegraph.com/subgraphs/name/ijlal-ishaq/social-blocks-subgraph",
+    {
+      query: `
+      {
+        posts(where:{id:"0x${id}"}){
+          transferHistory
+        }
+      }
+    `,
+    }
+  );
+
+  let transferArr = result?.data?.data?.posts[0]?.transferHistory;
+
+  if (transferArr) {
+    let users = await User.find({
+      address: {
+        $in: transferArr,
+      },
+    });
+    let usersInOrder = [];
+
+    if (users) {
+      users.forEach((user) => {
+        usersInOrder[transferArr.indexOf(user.address.toLowerCase())] = user;
+      });
+    }
+
+    res.json({
+      usersInOrder,
+    });
+  } else {
+    res.json({ message: "post not found." });
   }
 };
 
@@ -109,4 +166,6 @@ module.exports = {
   getPosts: getPosts,
   getUserPosts: getUserPosts,
   getSinglePost,
+  removeAllPostsAndUsers,
+  getTransferHistory,
 };
