afeda2bfbb161e984a4bc1f74b5d3db8a155d0d4
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-04-14 15:19:10 +0500

    changes

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,26 +1,26 @@
-const Web3 = require("web3");
-const keccak = require("keccak256");
-const User = require("../models/user");
+const Web3 = require('web3');
+const keccak = require('keccak256');
+const User = require('../models/user');
 
-const web3 = new Web3("https://rpc-mumbai.maticvigil.com/");
+const web3 = new Web3('https://rpc-mumbai.maticvigil.com/');
 
 module.exports = {
   verifySignature: async (req, res, next) => {
     try {
-      const timeConstant = 3600;
+      const timeConstant = 86400;
 
       const time = Math.floor(Math.floor(Date.now() / 1000) / timeConstant);
-      const hash = keccak(time.toString()).toString("hex");
+      const hash = keccak(time.toString()).toString('hex');
 
       const recoveredAddress = web3.eth.accounts.recover(
         hash,
-        req.body.signature
+        req.body.signature,
       );
 
       if (
         req.body.userAddress.toLowerCase() !== recoveredAddress.toLowerCase()
       ) {
-        res.send("Invalid signature");
+        res.send('Invalid signature');
       } else {
         const user = await User.findOne({ address: req.body.userAddress });
 
@@ -28,7 +28,7 @@ module.exports = {
         next();
       }
     } catch (e) {
-      res.send("Invalid signature");
+      res.send('Invalid signature');
     }
   },
 };
