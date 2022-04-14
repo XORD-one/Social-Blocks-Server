5cd46696908d7476fb5f67bc632f7459f9f2883c
ijlal-ishaq <ijlalishaq2019@gmail.com>
2022-04-15 02:18:51 +0500

    changes

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,7 @@ const PostsController = require("../controllers/post_controller");
 router.get("/getPosts/:address", PostsController.getPosts);
 router.get("/getUserPosts", PostsController.getUserPosts);
 router.get("/getSinglePost/:id", PostsController.getSinglePost);
-router.get("/delete", PostsController.removeAllPostsAndUsers);
+// router.get("/delete", PostsController.removeAllPostsAndUsers);
 router.get("/getTransferHistory/:id", PostsController.getTransferHistory);
 
 module.exports = {
